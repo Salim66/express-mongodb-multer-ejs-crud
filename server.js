a760e10ab0437bdf1d3f2bc8e850cc9a0867cf38
Salim66 <salimhasanriad8@gmail.com>
2022-06-26 22:17:39 +0600

    ejs template engine setu

--- server.js
+++ server.js
@@ -10,12 +10,25 @@ const path = require('path');
 const PORT = process.env.SERVER_P0RT || 5000;
 
 // Ejs Setup
+app.set("view engine", 'ejs');
 
 // Express js static folder 
-app.use(express.static(path.join(__dirname, '/public')));
+app.use('/assets', express.static(path.join(__dirname, '/assets')));
 
 // Routes
+app.get('/', (req, res) => {
+    res.render('home', {
+        title : 'This is load from express ejs',
+        error : {
+            status : false,
+            msg : 'This si error'
+        }
+    });
+});
 
+app.get('/about', (req, res) => {
+    res.render('about');
+});
 
 // Server Listen
 app.listen(PORT, () => console.log(`Server is running port is ${PORT}`));
